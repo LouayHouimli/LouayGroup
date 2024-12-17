61bf8cb10616f5f8082a3118a0aea41c85f2dd52
Louay Houimli <louayhouimli@engineer.com>
2024-12-17 14:41:12 +0100

    for vercel deploy

--- components/project-switcher.tsx
+++ components/project-switcher.tsx
@@ -128,7 +128,7 @@ export function TeamSwitcher() {
           <DialogHeader>
             <DialogTitle>Add Team</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
+              Make changes to your profile here. Click save when you&apos;re done.
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4 py-4">
